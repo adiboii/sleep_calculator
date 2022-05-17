b6cfa11488cc8880259d0922a5937908bc41ed9c
adi.boyii <adireyes00@gmail.com>
2022-05-17 23:19:19 +0800

    added apptheme

---
+++ lib/themes/AppTheme.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+ThemeData AppThemeData = ThemeData(
+  scaffoldBackgroundColor: Color(0xFF21263A),
+  textTheme: TextTheme(
+    headline1: TextStyle(
+      fontSize: 40,
+      fontFamily: "Outfit",
+      color: Colors.white,
+    ),
+    headline2: TextStyle(
+      fontSize: 28,
+      fontFamily: "Hind",
+      fontWeight: FontWeight.w800,
+      color: Colors.white,
+    ),
+    headline3: TextStyle(
+      fontSize: 28,
+      fontFamily: "Hind",
+      fontWeight: FontWeight.w800,
+      color: Colors.white,
+    ),
+    headline4: TextStyle(
+      fontFamily: "Outfit",
+      fontSize: 30,
+      color: Colors.white,
+    ),
+    headline5: TextStyle(
+      fontFamily: "Hind",
+      fontSize: 20,
+      fontWeight: FontWeight.w200,
+      color: Colors.white,
+    ),
+  ),
+);
